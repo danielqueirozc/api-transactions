10a7228a3c1299f1d948d0d19977b48d2d3b1e08
danielqueirozc <danielqueiroz.job@gmail.com>
2025-02-15 16:36:26 -0300

    test: created test E2E

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,11 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else (
+  config()
+)
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
@@ -19,3 +25,5 @@ export const env = _env.data
 // parse: pega o process e verifica se tem o forrmato certo de acordo com o schema && dispara && para o codigo
 // safeParse: pega o process e verifica se tem o forrmato certo de acordo com o schema && nao dispara um erro && nao para o codigo
 // enum: uma entre algumas opcoes
+
+//nao e obrigatorio colocar o NODE_ENV=test porque o vitest preenche automaticamente (somente quando estou executando os testes) no arquivo .env.test
--- test/example.test.ts
+++ test/example.test.ts
@@ -1,8 +1,15 @@
-import { expect, test, beforeAll, afterAll, describe } from 'vitest'
+import { expect, test, beforeAll, afterAll, describe, beforeEach } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
+import { execSync } from 'node:child_process'
 
 describe('Transactions routes', () => {
+    // beforeAll(() => {
+    //     execSync('npm run knex migrate:rollback --all')
+    //     execSync('npm run knex migrate:latest')
+
+    // })
+    
     beforeAll(async () => {
         await app.ready()
     }) // antes de rodar os testes esse funcao aguarda o app ser iniciado, depois disso os testes podem ser executados
@@ -10,6 +17,12 @@ describe('Transactions routes', () => {
     afterAll(async () => {
         await app.close()
     }) // depois de rodar os testes esse funcao fecha o app
+
+    beforeEach(() => {
+        execSync('npm run knex migrate:rollback --all') // apaga o banco de dados
+        // execSync('npm run knex migrate:up 20250202184343_create-transactions.ts') // para executar uma migration especifica
+        execSync('npm run knex migrate:latest') // cria o banco de dados
+    })
     
     test('should be able to create a new transaction', async () => {
     
@@ -50,6 +63,54 @@ describe('Transactions routes', () => {
                 amount: 5000,
             })
         ])
+    })
+
+    test('should be able to get especific transaction', async () => {
+
+        const createTransactionResponse = await request(app.server).post('/transactions').send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+        
+        const cookies = createTransactionResponse.get('Set-Cookie')
+       
+
+        const listTransactionsResponse = await request(app.server).get('/transactions').set('Cookie', cookies?.[0] ?? '').expect(200) 
+        const transactionId = listTransactionsResponse.body.transactions[0].id
+        const getTransactionResponse = await request(app.server).get(`/transactions/${transactionId}`).set('Cookie', cookies?.[0] ?? '').expect(200) 
+
+       
+        expect(getTransactionResponse.body.transaction).toEqual(
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000,
+            })
+        )
+    })
+
+    test('should be able to list all transactions', async () => {
+
+        const createTransactionResponse = await request(app.server).post('/transactions').send({
+            title: 'New transaction',
+            amount: 5000,
+            type: 'credit'
+        })
+        
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        await request(app.server).post('/transactions').set('Cookie', cookies?.[0] ?? '').send({
+            title: 'New transaction',
+            amount: 3000,
+            type: 'debit'
+        })
+       
+        const summaryResponse = await request(app.server).get('/transactions/summary').set('Cookie', cookies?.[0] ?? '').expect(200) 
+        
+
+        expect(summaryResponse.body.summary).toEqual({
+            amount: 2000,
+        })
     })
 })
 
